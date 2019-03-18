6fcd22cba3f6ce1db7d34333c7e47e0aa87d9363
chloele33 <chloele@seas.upenn.edu>
2019-03-17 23:11:08 -0400

    Kernel Reg setup

---
+++ KernelReg.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import math
+import pandas as pd
+
+import sklearn
+
+# assuming using two points for bounding box
+# assuming 2D points defined by (x, y)
+
+MrrBoundingBox1 = np.array([[0,3], [3, 0]])
+MrrBoundingBox2 = np.array([[2,5], [7, 2]])
+
+SinkBoundingBox1 = np.array([0, 6], [3, 4])
+SinkBoundingBox2 = np.array([2, 5], [7, 3])
+
+leftWall1 = -2
+rightWall1 = 8
+
+
+leftWall2 = -3
+rightWall2 = 12
+
+ceiling1 = -2
+ceiling2 = 0
+
+#extract feature sets:
+#a
+#b
+#c
+#d
+
+
+
+
+
+
+
+
+
