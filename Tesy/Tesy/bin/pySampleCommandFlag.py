162ea53087914c13ea453b6976b7af2b54337ddc
Iciar Andreu <iciar.andreu@gmail.com>
2019-04-29 19:11:17 -0700

    helper relationships

--- Tesy/Tesy/bin/pySampleCommandFlag.py
+++ Tesy/Tesy/bin/pySampleCommandFlag.py
@@ -15,6 +15,9 @@ kPluginCmdName = 'polyX'
 kShortFlagGroup = '-g'
 kLongFlagGroup = '-group'
 
+kShortFlagGroupProp = '-gp'
+kLongFlagGroupProp = '-groupProp'
+
 kShortFlagLabel = '-l'
 kLongFlagLabel = '-label'
 
@@ -34,10 +37,17 @@ kLongFlagYMax = '-yMax'
 kShortFlagPropLabel = '-p'
 kLongFlagPropLabel = '-propLabel'
 
+kShortFlagResult = '-r'
+kLongFlagResult = '-result'
+
 #Prop_label = 'mirror'
 
 
-exampleObjects = []
+exampleScenes = []
+exampleVecs = []
+
+propagateScenes = []
+propagateVecs = []
 
 
 
@@ -76,6 +86,7 @@ class MyCommandWithFlagClass( OpenMaya.MPxCommand ):
         # The following MArgParser object allows you to check if specific flags are set.
         argData = OpenMaya.MArgParser( self.syntax(), args )
         
+        # Get the information for the example scenes
         if argData.isFlagSet( kShortFlagGroup ) and argData.isFlagSet(kShortFlagLabel) and argData.isFlagSet(kShortFlagXMin) \
             and argData.isFlagSet(kShortFlagXMax) and argData.isFlagSet(kShortFlagYMin) and argData.isFlagSet(kShortFlagYMax):
             maya.mel.eval("print \"It Works!\"")
@@ -94,21 +105,61 @@ class MyCommandWithFlagClass( OpenMaya.MPxCommand ):
             #print kLongFlagName + ': ' + str( flagValue )
             groupNum = int(group[-1])
             if (groupNum > 0):
-            	if (groupNum > len(exampleObjects)):
-            		for x in range(len(exampleObjects), groupNum):
+            	if (groupNum > len(exampleScenes)):
+            		for x in range(len(exampleScenes), groupNum):
             			scene = sim.Scene("Group" + str(x))
-            			exampleObjects.append(scene)
+            			vec = sim.VecPoly()
+            			exampleScenes.append(scene)
+            			exampleVecs.append(vec)
             	#scene = sim.Scene(group)
-            	minVec = sim.vec2(xmin, ymin)
-            	maxVec = sim.vec2(xmax, ymax)
-            	poly = sim.Polygon(sim.vec2(xmin, ymin), sim.vec2(xmax, ymax), str(label))
-            	exampleObjects[groupNum - 1].addPolygon(poly)
+            	#poly = sim.Polygon(sim.vec2(xmin, ymin), sim.vec2(xmax, ymax), str(label))
+            	exampleScenes[groupNum - 1].addPolygon(xmin, ymin, xmax, ymax, str(label), exampleVecs[groupNum - 1])
+
+
+        # Get the information for the scenes we will be propagating
+        if argData.isFlagSet( kShortFlagGroupProp ) and argData.isFlagSet(kShortFlagLabel) and argData.isFlagSet(kShortFlagXMin) \
+            and argData.isFlagSet(kShortFlagXMax) and argData.isFlagSet(kShortFlagYMin) and argData.isFlagSet(kShortFlagYMax):
+            maya.mel.eval("print \"It Works!\"")
+            group = argData.flagArgumentString(kShortFlagGroupProp, 0)
+            label = argData.flagArgumentString(kShortFlagLabel, 0)
+            xmin = argData.flagArgumentFloat(kShortFlagXMin, 0)
+            xmax = argData.flagArgumentFloat(kShortFlagXMax, 0)
+            ymin = argData.flagArgumentFloat(kShortFlagYMin, 0)
+            ymax = argData.flagArgumentFloat(kShortFlagYMax, 0)
+            maya.mel.eval("print \"Group: " + group + "\"")
+            maya.mel.eval("print \"Label: " + label + "\"")
+            maya.mel.eval("print \"Xmin: " + str(xmin) + "\"")
+            # In this case, we print the passed flag's value as an integer.
+            # We use the '0' to index the flag's first and only parameter.
+            #flagValue = argData.flagArgumentInt( kShortFlagName, 0 )
+            #print kLongFlagName + ': ' + str( flagValue )
+            groupNum = int(group[-1])
+            if (groupNum > 0):
+            	if (groupNum > len(propagateScenes)):
+            		for x in range(len(propagateScenes), groupNum):
+            			scene = sim.Scene("Group" + str(x))
+            			vec = sim.VecPoly()
+            			propagateScenes.append(scene)
+            			propagateVecs.append(vec)
+            	#scene = sim.Scene(group)
+            	#poly = sim.Polygon(sim.vec2(xmin, ymin), sim.vec2(xmax, ymax), str(label))
+            	propagateScenes[groupNum - 1].addPolygon(xmin, ymin, xmax, ymax, str(label), propagateVecs[groupNum - 1])
 
         if argData.isFlagSet(kShortFlagPropLabel):
         	propLabel = argData.flagArgumentString(kShortFlagPropLabel, 0)
-        	for i in range(0, len(exampleObjects) - 1):
-        		exampleObjects[i].desiredLabel = propLabel
+        	for i in range(0, len(exampleScenes) - 1):
+        		exampleScenes[i].desiredLabel = str(propLabel)
         	maya.mel.eval("print \"It Works!\"")
+
+        if argData.isFlagSet(kShortFlagResult):
+        	results = [2.0, 1.0, 4.5, 6.2]
+        	centerX = (results[0] + results[2]) / 2
+        	centerY = (results[1] + results[3]) / 2
+        	scaleX = results[2] - centerX
+        	scaleY = results[3] - centerY
+        	maya.mel.eval("duplicate -un;")
+        	maya.mel.eval("move -x " + str(centerX) + " -y " + str(centerY) +";")
+        	maya.mel.eval("scale -x " + str(scaleX) + " -y " + str(scaleY) + ";")
             
             
         
@@ -134,6 +185,10 @@ def syntaxCreator():
     syntax.addFlag( kShortFlagYMax, kLongFlagYMax, OpenMaya.MSyntax.kDouble )
 
     syntax.addFlag( kShortFlagPropLabel, kLongFlagPropLabel, OpenMaya.MSyntax.kString )
+
+    syntax.addFlag( kShortFlagGroupProp, kLongFlagGroupProp, OpenMaya.MSyntax.kString )
+
+    syntax.addFlag( kShortFlagResult, kLongFlagResult, OpenMaya.MSyntax.kDouble )
     
     # ... Add more flags here ...
         
